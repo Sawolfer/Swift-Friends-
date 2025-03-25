9ed092df4531bd47412b69e45a8d3c3f7e2baa8d
tttuuu13 <tim.ib.222005@gmail.com>
2025-03-26 00:20:03 +0300

    UIView+Pin extension added

---
+++ Friends/Friends/Components/ScheduleSliders/ScheduleSlider.swift
@@ -0,0 +1,8 @@
+//
+//  ScheduleSlider.swift
+//  Friends
+//
+//  Created by тимур on 25.03.2025.
+//
+
+import Foundation
---
+++ Friends/Friends/Components/ScheduleSliders/TimeSlotSlider.swift
@@ -0,0 +1,8 @@
+//
+//  TimeSlotSlider.swift
+//  Friends
+//
+//  Created by тимур on 26.03.2025.
+//
+
+import Foundation
